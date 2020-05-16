2d1e3a4d8493c9ff36933d88834914a3162d5024
ykCyber <yusufkenan2604@gmail.com>
2020-05-17 00:40:20 +0300

    commit

---
+++ src/test/java/com/vytrack/tests/Emoji.java
@@ -0,0 +1,3 @@
+package com.vytrack.tests
+public class Emoji {
+}
--- src/test/java/com/vytrack/tests/LoginTest.java
+++ src/test/java/com/vytrack/tests/LoginTest.java
@@ -1,20 +1,20 @@
-//package com.vytrack.tests;
-//
-//import com.vytrack.utilities.Login;
-//import com.vytrack.utilities.Util;
-//import com.vytrack.utilities.WebDriverFactory;
-//import org.openqa.selenium.WebDriver;
-//import org.testng.annotations.Test;
-//
-//public class LoginTest {
-//    private static final String URL = "https://qa1.vytrack.com/";
-//    @Test
-//    public void test() throws Exception {
-//       String filePath = "C:\\Users\\mksimsir\\Documents\\testData.xlsx";
+package com.vytrack.tests;
+
+import com.vytrack.utilities.Login;
+import com.vytrack.utilities.Util;
+import com.vytrack.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    private static final String URL = "https://qa1.vytrack.com/";
+    @Test
+    public void test() throws Exception {
+       String filePath = "C:\\Users\\mksimsir\\Documents\\testData.xlsx";
 //        String[][] dataFromExcel = Util.getDataFromExcel(filePath, "Data", "Table");
-//        for (String[] strings : dataFromExcel) {
-//            System.out.println("strings = " + strings);
-//        }
-//    }
-//}
-//
+        for (String[] strings : dataFromExcel) {
+            System.out.println("strings = " + strings);
+        }
+    }
+}
+
